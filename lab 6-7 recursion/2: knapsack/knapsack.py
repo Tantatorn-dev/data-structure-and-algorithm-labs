c6f9ce5d915fb1c53832d0708a439a9710b26dc4
Tantatorn <timefear007@gmail.com>
2019-10-28 05:09:27 +0700

    add delete method in BST

---
+++ lab 6-7 recursion/2: knapsack/knapsack.py
@@ -0,0 +1,32 @@
+
+def printSack(prices,maxI):
+    global B
+    for i in range(maxI+1):
+        print(B[prices[i]],end=' ')
+    print()
+
+def pick(sack,i,moneyLeft,ig):
+    global N
+    global B
+    if ig<N:
+       price = B[ig]
+       if moneyLeft < price:
+           pick(sack, i, moneyLeft,ig+1)
+       else:
+            moneyLeft -= price
+            sack[i] = ig
+            if moneyLeft == 0:
+                printSack(sack,i)
+            else:
+                pick(sack,i+1,moneyLeft,ig+1)
+            pick(sack,i,moneyLeft+price,ig+1)
+
+k=20
+B=[20,10,5,5,3,2,20,10]
+prices = B
+N=len(B)
+mLeft = 20
+sack = N*[-1]
+i= 0
+ig = 0
+pick(sack,i,mLeft,ig)
--- lab 8 Dynamic Binary Tree/bst/bst.py
+++ lab 8 Dynamic Binary Tree/bst/bst.py
@@ -7,12 +7,13 @@ class Node:
     def __str__(self):
         return str(self.data)
 
+
 class BST:
 
-    def __init__(self,root=None):
-        self.root = root 
+    def __init__(self, root=None):
+        self.root = root
 
-    def addI(self,data):
+    def addI(self, data):
         if self.root is None:
             self.root = Node(data)
         else:
@@ -20,27 +21,27 @@ class BST:
             p = self.root
             while p is not None:
                 parent_p = p
-                p = p.left if data<p.data else p.right
-            
+                p = p.left if data < p.data else p.right
+
             if data < parent_p.data:
                 parent_p.left = Node(data)
             else:
                 parent_p.right = Node(data)
-    
-    def add(self,data):
-        self.root = BST._add(self.root,data)
+
+    def add(self, data):
+        self.root = BST._add(self.root, data)
 
     @staticmethod
-    def _add(root,data):
+    def _add(root, data):
         if root is None:
             return Node(data)
         else:
-            if data<root.data:
-                root.left = BST._add(root.left,data)
+            if data < root.data:
+                root.left = BST._add(root.left, data)
             else:
-                root.right = BST._add(root.right,data)
+                root.right = BST._add(root.right, data)
             return root
-    
+
     def inOrder(self):
         BST._inOrder(self.root)
         print()
@@ -49,24 +50,120 @@ class BST:
     def _inOrder(root):
         if root is not None:
             BST._inOrder(root.left)
-            print(root.data,end=' ')
+            print(root.data, end=' ')
             BST._inOrder(root.right)
-    
+
     def printSideway(self):
-        BST._printSideway(self.root,0)
+        BST._printSideway(self.root, 0)
         print()
-    
+
     @staticmethod
-    def _printSideway(root,level):
+    def _printSideway(root, level):
         if root is not None:
-            BST._printSideway(root.right,level+1)
-            print(' '*level,root.data,sep='')
-        BST._printSideway(root.left,level+1)
-        
-    
+            BST._printSideway(root.right, level+1)
+            print("   "*level, end="")
+            print(root.data)
+            BST._printSideway(root.left, level+1)
+
+    def preOrder(self):
+        BST._preOrder(self.root)
+        print()
+
+    @staticmethod
+    def _preOrder(root):
+        if root is not None:
+            print(root.data, end=" ")
+            BST._preOrder(root.left)
+            BST._preOrder(root.right)
+
+    def postOrder(self):
+        BST._postOrder(self.root)
+        print()
+
+    @staticmethod
+    def _postOrder(root):
+        if root is not None:
+            BST._postOrder(root.left)
+            BST._postOrder(root.right)
+            print(root.data, end=" ")
+
+    def search(self, data):
+        return BST._search(self.root, data)
+
+    @staticmethod
+    def _search(root, data):
+        if root is None or root.data == data:
+            return root
+        if data < root.data:
+            return BST._search(root.left, data)
+        else:
+            return BST._search(root.right, data)
+
+    def path(self, data):
+        BST._path(self.root, data)
+
+    @staticmethod
+    def _path(root, data):
+        if root is None or root.data == data:
+            print(root.data)
+            return
+        if data < root.data:
+            print(root.data, end="->")
+            BST._path(root.left, data)
+        else:
+            print(root.data, end="->")
+            BST._path(root.right, data)
+
+    def delete(self, data):
+        if self.root is None:
+            return
+        BST._delete(self.root, data)
+
+    @staticmethod
+    def minValueNode(node):
+        current = node
+
+        while(current.left is not None):
+            current = current.left
+
+        return current
+
+    @staticmethod
+    def _delete(root, data):
+        if root is None:
+            return root
+
+        if data < root.data:
+            root.left = BST._delete(root.left, data)
+
+        elif(data > root.data):
+            root.right = BST._delete(root.right, data)
+
+        else:
+
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = BST.minValueNode(root.right)
+
+            root.data = temp.data
+
+            root.right = BST._delete(root.right, temp.data)
+
+
 t = BST()
 t.addI(10)
 t.addI(5)
 t.addI(2)
 t.addI(3)
-t.inOrder()
+t.addI(23)
+t.addI(32)
+t.delete(10)
+t.printSideway()
