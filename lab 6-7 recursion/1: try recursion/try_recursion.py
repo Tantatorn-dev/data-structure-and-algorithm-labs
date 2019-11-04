ebc1cc27308ecc437da56254a3fc7747148d4c1c
Tantatorn <timefear007@gmail.com>
2019-11-05 02:18:35 +0700

    add tower of hanoi

--- lab 6-7 recursion/1: try recursion/try_recursion.py
+++ lab 6-7 recursion/1: try recursion/try_recursion.py
@@ -122,3 +122,12 @@ for i in range(10):
 
 printList(head)
 """
+
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
+    if n == 1: 
+        print ("Move disk 1 from rod",from_rod,"to rod",to_rod )
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod) 
+    print ("Move disk",n,"from rod",from_rod,"to rod",to_rod )
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod) 
+    
